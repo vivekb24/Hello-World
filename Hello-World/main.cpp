9759d5903a18181b0ed2f91071c242ab5962f3ee
vivekb24 <bass.vivek@gmail.com>
2020-10-20 00:34:44 +0530

    Added qDebug message Hello-World

--- Hello-World/main.cpp
+++ Hello-World/main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
+    qDebug()<<"Hello World";
     return a.exec();
 }
